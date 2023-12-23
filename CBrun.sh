1c1d744ce0d9ff7ebc20e86065db7dd87e17fdd1
Denver Noell <denvernoell@gmail.com>
2023-12-23 14:08:50 -0800

    Updated gitignore

--- CBrun.sh
+++ CBrun.sh
@@ -1,3 +1,6 @@
 # cd "//ppeng.com/pzdata/docs/Project Resources/Ag Water/apps/district_management/"
-cd "G:/Cucamonga Valley WD - 4026/402622002-Cucmonga Basin Safe Yield Study/400 GIS/Scripts/testing"
-/c/Users/Denver/AppData/Local/pypoetry/Cache/virtualenvs/streamlit-data-management-cgCxCbpY-py3.10/Scripts/python.exe -m streamlit run "cucamonga_app.py" --server.port 91
+# cd "G:/Cucamonga Valley WD - 4026/402622002-Cucmonga Basin Safe Yield Study/400 GIS/Scripts/testing"
+# C:\Users\Denver\OneDrive - PROVOST & PRITCHARD CONSULTING GROUP\Desktop\DN\Projects\Cucamonga\testing\cucamonga_app.py
+cd "C:/Users/Denver/OneDrive - PROVOST & PRITCHARD CONSULTING GROUP/Desktop/DN/Projects/Cucamonga/testing"
+/c/Users/Denver/AppData/Local/pypoetry/Cache/virtualenvs/streamlit-data-management-cgCxCbpY-py3.10/Scripts/python.exe -m streamlit run "cucamonga_app.py" --server.port 92
+
